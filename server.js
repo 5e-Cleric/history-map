dfbc0047c234cb5af6dd82499a5e41467a385978
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-09-24 20:32:41 +0200

    db connection

--- server.js
+++ server.js
@@ -7,41 +7,48 @@ import eventRoutes from './backend/routes/eventRoutes.js';
 const app = express();
 const PORT = 3050;
 
+const dbCreds = { username: 'history-map' , password: 'QFIDyC22jHsR9ubj' };
+// MongoDB connection string with credentials and database name
+const mongoURI = `mongodb+srv://history-map:${dbCreds.password}@history-map.tjwf6.mongodb.net/historymap?retryWrites=true&w=majority`;
+
 // Middleware to handle CORS
 app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// MongoDB connection string
-const mongoURI = 'mongodb://127.0.0.1/historymap';
-
-const handleConnectionError = (error)=>{
-	if(error) {
-		console.error('Could not connect to a Mongo database: \n');
-		console.error(error);
-		console.error('\nIf you are running locally, make sure mongodb.exe is running and DB URL is configured properly');
-		process.exit(1); // non-zero exit code to indicate an error
-	}
+// Handle connection errors with a custom function
+const handleConnectionError = (error) => {
+    if (error) {
+        console.error('Could not connect to MongoDB: \n');
+        console.error(error);
+        console.error('\nEnsure MongoDB URI is configured properly and MongoDB is running.');
+        process.exit(1); // non-zero exit code to indicate an error
+    }
 };
 
+// Connect to MongoDB using Mongoose
 mongoose
-    .connect(mongoURI)
+    .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => console.log('MongoDB connected'))
-    .catch((error)=>handleConnectionError(error));
+    .catch((error) => handleConnectionError(error));
 
 // Define routes
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
+
 app.use((req, res, next) => {
-    //console.log(`Received request for ${req.url}`);
     next();
 });
 
 // Use the map routes
 app.use('/api/map', mapRoutes);
 
+// Use the event routes
 app.use('/api/event', eventRoutes);
 
 // Start the server
