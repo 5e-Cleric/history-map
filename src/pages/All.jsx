9a45f02bc2778e71b927d189a89bac7d71e10e3c
Víctor Losada Hernández <losada.hernandez.victor@gmail.com>
2024-09-24 21:55:27 +0200

    fix envs and add vite api url

--- src/pages/All.jsx
+++ src/pages/All.jsx
@@ -12,7 +12,7 @@ function All() {
 	}, []);
 
 	const fetchMaps = () => {
-		fetch('/api/map/all')
+		fetch(`${import.meta.env.VITE_API_URL}/api/map/all`)
 			.then((response) => response.json())
 			.then((data) => {
 				setmaps(data);
@@ -28,7 +28,7 @@ function All() {
 
 	const deleteMap = async (id) => {
 		try {
-			await fetch(`/api/map/${id}`, {
+			await fetch(`${import.meta.env.VITE_API_URL}/api/map/${id}`, {
 				method: 'DELETE',
 				headers: {
 					'Content-Type': 'application/json',
